f8a1b971cc5616673c78d1d1bfc389e27fa3ad70
TheCheeseMeister <ssomething511@gmail.com>
2024-07-24 18:55:53 -0400

    Added some base styling. Also added event listeners for the tab buttons - when pressed at the moment, they will remove the current content of the page and print out which page it'll navigate to.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import { createContent } from './pageLoad.js';
+import { createHome } from './tabs/home.js';
+import { createMenu } from './tabs/menu.js';
+import { createAbout } from './tabs/about.js';
+
 import "./styles.css";
 
-function component() {
+// Creates initial content of page
+
+function intialContent() {
     return createContent();
 }
 
-document.body.appendChild(component());
+document.body.appendChild(intialContent());
+
+// Buttons for switching tabs on page
+
+const buttons = document.getElementsByClassName("tab");
+
+for (let i = 0; i < buttons.length; i++) {
+    buttons.item(i).addEventListener("click", event => {
+        event.preventDefault();
+
+        document.body.removeChild(document.body.lastChild);
+        
+        switch(event.target.getAttribute('id')) {
+            case "home":
+                createHome();
+                break;
+            case "menu":
+                createMenu();
+                break;
+            case "about":
+                createAbout();
+                break;
+        }
+    });
+}
